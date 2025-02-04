3b60350a95f66e23643acfcba871cb3ef39a87c0
juanda <juandacorreo@gmail.com>
2025-02-04 12:38:29 +0100

    Ejercicio solucionado

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import "./style.css";
-
 document.querySelector("#app").innerHTML = `
     <button class="show-modal">Show modal 1</button>
 		<button class="show-modal">Show modal 2</button>
@@ -19,3 +18,47 @@ document.querySelector("#app").innerHTML = `
 		</div>
 		<div class="overlay hidden"></div>
 `;
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+
+// definimos la función closeModal
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+// definimos la función openModal
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+// asociamos la función openModal a cada uno de los botones usando forEach
+btnsOpenModal.forEach((btn) => btn.addEventListener("click", openModal));
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+// for (let i = 0; i < btnsOpenModal.length; i++) {
+//   btnsOpenModal[i].addEventListener("click", openModal);
+// }
+
+console.log(modal, overlay, btnCloseModal, btnsOpenModal);
+
+// const handleKeyDown = (e) => {
+//   if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+//     closeModal();
+//   }
+// };
+
+// if ESC key is pressed, close the modal
+document.addEventListener("keydown", handleKeyDown);
+
+function handleKeyDown(e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+}
